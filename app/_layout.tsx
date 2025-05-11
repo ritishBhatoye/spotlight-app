2350c328fa5d9e11a444e3b49ca3ec3b72ce8c10
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-11 22:01:27 +0530

    Update package dependencies to include expo-secure-store and refactor RootLayout to integrate ClerkProvider for improved authentication handling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import "../global.css";
-
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-
+import React from "react";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { SafeAreaView } from "react-native";
 
 export default function RootLayout() {
@@ -15,11 +16,13 @@ export default function RootLayout() {
   }
 
   return (
-    <SafeAreaView className="flex-1 bg-black">
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </SafeAreaView>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaView className="flex-1 bg-black">
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
