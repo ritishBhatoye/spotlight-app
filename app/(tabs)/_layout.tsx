f2184da1c006de257d330b86c2ec4fe5f7194ec2
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-11 15:59:54 +0530

    Rename notification tab to notifications for consistency in TabLayout.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,7 +37,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="notification"
+        name="notifications"
         options={{
           title: "Notification",
           tabBarIcon: ({ color, focused }) => (
