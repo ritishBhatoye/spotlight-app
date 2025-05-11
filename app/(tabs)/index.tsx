a5c087b81f037b3ba604d5c6bf82876d6d778968
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-11 15:44:50 +0530

    Increase font size of the Spotlight text in HomeScreen for better visibility.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView, Text } from "react-native";
 const HomeScreen = () => {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text className="text-red-500 text-2xl">Spotlight</Text>
+      <Text className="text-red-500 text-[50px]">Spotlight</Text>
     </SafeAreaView>
   );
 };
